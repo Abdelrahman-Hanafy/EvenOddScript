b2f4580445838ecbc9cce7eccd895a98ef0c0985
GalalMohammed <galalmoh2001@gmail.com>
2023-08-23 16:45:36 +0300

    implement isEven

--- Galal/even_odd_by_bitwise_and.sh
+++ Galal/even_odd_by_bitwise_and.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
+isEven()
+{
+	num=$1
+	if ((num&1));
+	then
+		echo Odd
+	else
+		echo Even
+	fi
+}
 ((x=0))
 while [ "$x" != 'stop' ];
 do
 	read -r -p "Enter x or 'stop' to stop: " x
 	if [ "$x" != "stop" ];
 	then
-		if ((x&1));
-		then
-			echo Odd
-		else
-			echo Even
-		fi
+		isEven x
 	fi
 done
--- Galal/even_odd_by_mod.sh
+++ Galal/even_odd_by_mod.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
+isEven ()
+{
+	num=$1
+	if ((num%2));
+	then
+		echo Odd
+	else
+		echo Even
+	fi
+}
 ((x=0))
 while [ "$x" != 'stop' ]; 
 do
 	read -r -p "Enter x or 'stop' to stop: " x
 	if [ "$x" != "stop" ];
 	then
-		if ((x%2));
-		then
-			echo Odd
-		elif ((x%2==0))
-		then
-			echo Even
-		fi
+		isEven x
 	fi
 done
