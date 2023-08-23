1000fa0152e0fcd970e895bb8239b115e9683d85
AhmedMohamed264 <medo.mohamed201958@gmail.com>
2023-08-23 20:36:13 +0300

    Add isEven function that takes the number as an argument then checks if is it even or odd using modulus operator and prints the result on the screen using echo inside the function

--- Ahmed_Mohamed/Even_Odd.sh
+++ Ahmed_Mohamed/Even_Odd.sh
@@ -1,10 +1,16 @@
+function isEven()
+{
+	if [ $(( $1 % 2 )) -eq 0 ]
+	then
+		echo "$1 is Even"
+	else
+		echo "$1 is Odd"
+	fi
+}
+
+
 echo "Enter a number: "
 
 read num
 
-if [ $((num % 2)) == 0 ]
-then
-	echo "$num is Even"
-else
-	echo "$num is Odd"
-fi
+isEven "$num"
