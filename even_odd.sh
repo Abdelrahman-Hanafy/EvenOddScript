e989cf0a88da55a21b7069a5016639ec1a38f206
Abdelrahman Ataa <72919098+abdelrahman-ops@users.noreply.github.com>
2023-08-23 13:49:16 +0300

    Implement Even or Odd script using Modulo Operator

--- even_odd.sh
+++ even_odd.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo "Enter a number:"
-read number
+read -p "Enter a number: " number
 
-if ((number % 2 == 0)); then
-    echo "$number is even."
+if [ $((number % 2)) -eq 0 ]; then
+    echo "The number is even."
 else
-    echo "$number is odd."
+    echo "The number is odd."
 fi
