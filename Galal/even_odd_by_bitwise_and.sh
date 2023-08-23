e790ae6280ebb8adba2dca05de1e7e21070379db
GalalMohammed <galalmoh2001@gmail.com>
2023-08-23 16:36:28 +0300

    add looping

--- Galal/even_odd_by_bitwise_and.sh
+++ Galal/even_odd_by_bitwise_and.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-read -r -p "Enter x: " x
-if ((x&1));
-then
-	echo Odd
-else
-	echo Even
-fi
+((x=0))
+while [ "$x" != 'stop' ];
+do
+	read -r -p "Enter x or 'stop' to stop: " x
+	if [ "$x" != "stop" ];
+	then
+		if ((x&1));
+		then
+			echo Odd
+		else
+			echo Even
+		fi
+	fi
+done
--- Galal/even_odd_by_mod.sh
+++ Galal/even_odd_by_mod.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
-read -r -p "Enter x: " x
-if ((x%2));
-then
-	echo Odd
-else
-	echo Even
-fi
+((x=0))
+while [ "$x" != 'stop' ]; 
+do
+	read -r -p "Enter x or 'stop' to stop: " x
+	if [ "$x" != "stop" ];
+	then
+		if ((x%2));
+		then
+			echo Odd
+		elif ((x%2==0))
+		then
+			echo Even
+		fi
+	fi
+done
