cc5070e50499fc6ff99aac2158a33631fb846e00
Youssef292 <youssefabdelhakem1@gmail.com>
2023-08-23 19:46:28 +0300

    Created the main structer

---
+++ Youssef/CheckNumber.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Check it's even or odd  using bitwise 
+
+echo "Enter first Nubmer"
+
+read Num
+
+if (( $Num&1));
+then
+      echo "This Odd Number"
+else
+      echo "This is Even Number"
+fi
+
+
+
