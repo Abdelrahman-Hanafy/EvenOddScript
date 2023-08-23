f94730015da1ea6a8736ea3c6bcee7a889e3cb21
Amr789 <amr.ayman.galal@gmail.com>
2023-08-23 20:52:53 +0300

    make the program contionuous till the user exit

--- Amr/Even_odd.sh
+++ Amr/Even_odd.sh
@@ -1,2 +1,19 @@
-read num
-echo $num
+#!/bin/bash
+
+is_even_odd() {
+    if (( $1 % 2 == 0 )); then
+        echo "even"
+    else
+        echo "odd"
+    fi
+}
+
+while true; do
+    read -p "Enter a number (or 'exit' to quit): " input
+
+    # Check if user wants to exit
+    if [[ "$input" == "exit" ]]; then
+        echo "Exiting the program."
+        break
+    fi
+done
